251ed5b3081f3bb4489e85d1dec428027d7973e7
Renan Cargnin <renansep93@hotmail.com>
2013-04-10 19:55:18 -0300

    all

---
+++ src/Level.js
@@ -0,0 +1,19 @@
+var Level = function(vid)
+{
+	var matrix = new Array(15);
+	for (var i=0; i<matrix.length; i++)
+		matrix[i] = new Array(12);
+			
+	for (var i=0; i<matrix.length; i++)
+	{
+		for (var j=0; j<matrix[i].length; j++)
+			matrix[i][j] = 1;
+	}
+		
+	return matrix;
+		
+	if (vid == 1)
+	{
+		
+	}
+}
